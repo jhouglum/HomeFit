d45490b84e886513930f1b6f54cd977770cbba35
jhouglum <joshua.houglum@gmail.com>
2017-04-27 22:01:31 -0400

    Commit 7 Added partially implemented billing list objects. Doing a commit before I implements nav buttons and everything goes boom.

--- app/src/main/java/com/homefit/android/homefit/BillListFragment.java
+++ app/src/main/java/com/homefit/android/homefit/BillListFragment.java
@@ -1,68 +1,168 @@
 package com.homefit.android.homefit;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 
-public class BillListFragment extends Fragment {
+public class BillListFragment extends Fragment implements View.OnClickListener, AdapterView.OnItemClickListener {
 
 //	private OnFragmentInteractionListener mListener;
 
-	public BillListFragment() {
-		// Required empty public constructor
-	}
+	private RecyclerView mBillingRecyclerView;
+	private BillingAdapter mBillingAdapter;
 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 													 Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.fragment_bill_list, container, false);
+		View view = inflater.inflate(R.layout.temp_bill_list_view, container, false);
+		ListView tempList = (ListView) view.findViewById(R.id.temp_billing_list_view); //replace
+		ArrayAdapter tempArrayAdapter = ArrayAdapter.createFromResource(getActivity(), //replace
+					R.array.temp_bill_list, android.R.layout.simple_list_item_1); //replace
+		tempList.setAdapter(tempArrayAdapter); //replace
+		tempList.setOnItemClickListener(this); //replace
 
-		TextView textView = (TextView) v.findViewById(R.id.list_item_bill);
-		textView.setText(R.string.hello_blank_fragment);
-		return v;
+//		mBillingRecyclerView = (RecyclerView) view.findViewById(R.id.billing_recycler_view);
+//		mBillingRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+//
+//		updateUI();
+
+		return view;
 	}
 
-//	// TODO: Rename method, update argument and hook method into UI event
-//	public void onButtonPressed(Uri uri) {
-//		if (mListener != null) {
-//			mListener.onFragmentInteraction(uri);
-//		}
-//	}
 
-//	@Override
-//	public void onAttach(Context context) {
-//		super.onAttach(context);
-//		if (context instanceof OnFragmentInteractionListener) {
-//			mListener = (OnFragmentInteractionListener) context;
-//		} else {
-//			throw new RuntimeException(context.toString()
-//				+ " must implement OnFragmentInteractionListener");
-//		}
-//	}
 
 	@Override
-	public void onDetach() {
-		super.onDetach();
-//		mListener = null;
+	public void onResume() {
+		super.onResume();
+		updateUI();
 	}
 
-	/**
-	 * This interface must be implemented by activities that contain this
-	 * fragment to allow an interaction in this fragment to be communicated
-	 * to the activity and potentially other fragments contained in that
-	 * activity.
-	 * <p>
-	 * See the Android Training lesson <a href=
-	 * "http://developer.android.com/training/basics/fragments/communicating.html"
-	 * >Communicating with Other Fragments</a> for more information.
-	 */
-//	public interface OnFragmentInteractionListener {
-//		// TODO: Update argument type and name
-//		void onFragmentInteraction(Uri uri);
-//	}
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		super.onCreateOptionsMenu(menu, inflater);
+		inflater.inflate(R.menu.billing_menu, menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+//			case R.id.menu_item_new_session:
+//				Session session = new Session();
+//				SessionList.get(getActivity()).addSession(session);
+//				Intent intentNew = SessionActivity.newIntent(getActivity(),
+//					session.getSessId());
+//				startActivity(intentNew);
+//				return true;
+			case R.id.menu_item_logoff:
+				Toast.makeText(getActivity(), R.string.msg_logging_off, Toast.LENGTH_SHORT).show();
+				if (LoginActivity.class == null) {
+					Intent intentLogin = new Intent(getActivity(), LoginActivity.class);
+					startActivity(intentLogin);
+				}
+				getActivity().finish();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private void updateUI() {
+//		BillListActivity billList = BillingList(getActivity().);
+//		List<String> bills = getResources(R.array.temp_bill_list);
+
+//		if (mBillingAdapter == null) {
+//			mBillingAdapter = new BillingAdapter(sessions);
+//			mSessionRecyclerView.setAdapter(mSessionsAdapter);
+//		} else {
+//			mSessionsAdapter.notifyDataSetChanged();
+//		}
+	}
+
+	@Override
+	public void onClick(View v) { //replace
+
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) { //replace
+
+	}
+
+	private class BillHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+		private TextView mBillTextView;
+
+		private Bill mBill;
+
+		public BillHolder(View itemView) {
+			super(itemView);
+			itemView.setOnClickListener(this);
+
+			mBillTextView = (TextView) itemView.findViewById(R.id.list_item_billing_text_view);
+
+		}
+
+		public void bindBill(Bill bill) {
+			mBill = bill;
+			mBillTextView.setText(R.string.temp_bill_list_item_text); //replace
+		}
+
+		@Override
+		public void onClick(View v) {
+			Intent intent = BillActivity.newIntent(getActivity(), mBill.getBillId());
+			startActivity(intent);
+		}
+	}
+
+	private class BillingAdapter extends RecyclerView.Adapter<BillHolder> {
+
+		private List<Bill> mBills;
+
+		public BillingAdapter(List<Bill> bills) {
+			mBills = bills;
+		}
+
+		@Override
+		public BillHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+			View view = layoutInflater.inflate(R.layout.list_item_billing,
+				parent, false);
+			return new BillHolder(view);
+		}
+
+		@Override
+		public void onBindViewHolder(BillHolder holder, int position) {
+			Bill bill = mBills.get(position);
+			holder.bindBill(bill);
+		}
+
+		@Override
+		public int getItemCount() {
+			return mBills.size();
+		}
+	}
 }
---
+++ app/src/main/java/com/homefit/android/homefit/BillingList.java
@@ -0,0 +1,13 @@
+package com.homefit.android.homefit;
+
+/**
+ * Created by joshu on 4/27/2017.
+ */
+
+public class BillingList {
+
+	public BillingList get() {
+		return new BillingList();
+	}
+
+}
